86fca849d7ada588bc3bb888cdd43679f240b810
Shreyas Minocha <sm-git@riseup.net>
2022-07-16 15:11:47 -0400

    Improve bundling
    - Suppress a warning
    - Rename some output files
    - Remove redundant declaration files
    
    Address #32

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,19 @@
+// @rollup/plugin-typescript: faster, but I couldn't get it to properly emit declaration files
+// rollup-plugin-typescript2: couldn't get it to respect output.file instead of tsconfig.json
 import typescript from 'rollup-plugin-ts';
+
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import polyfill from 'rollup-plugin-polyfill-node';
 import {terser} from 'rollup-plugin-terser';
+import pkg from './package.json';
 
 const clean = {comments: /[^\w\W]/};
 
 const builds = [
   {
     input: 'src/electionguard/index.ts',
-    output: {file: 'dist/index.js', format: 'esm'},
+    output: {file: pkg.main, format: 'esm'},
     plugins: [
       commonjs(),
       typescript(),
@@ -31,29 +35,26 @@ const builds = [
   },
   {
     input: 'src/electionguard/index.ts',
-    output: {file: 'dist/electionguard.mjs', format: 'esm'},
-    plugins: [
-      commonjs(),
-      resolve({preferBuiltins: false, browser: true}),
-      polyfill(),
-      typescript(),
-      terser({
-        output: clean,
-      }),
+    output: [
+      {file: pkg.browser, format: 'esm'},
+      {
+        file: 'dist/electionguard.umd.js',
+        name: 'eg',
+        format: 'umd',
+      },
     ],
-  },
-  {
-    input: 'src/electionguard/index.ts',
-    output: {
-      file: 'dist/electionguard.js',
-      name: 'eg',
-      format: 'umd',
-    },
+    context: 'this',
     plugins: [
       commonjs(),
       resolve({preferBuiltins: false, browser: true}),
       polyfill(),
-      typescript(),
+      typescript({
+        // somewhat hacky, but official, way to prevent redundant declaration files
+        hook: {
+          outputPath: (path, kind) =>
+            kind === 'declaration' ? './dist/index.d.ts' : path,
+        },
+      }),
       terser({
         output: clean,
       }),
